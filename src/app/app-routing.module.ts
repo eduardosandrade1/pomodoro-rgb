1e680b7af3bf35ad2e69b4c847cebb969ab618c9
Eduardo <eduardo@ezig.com.br>
2022-04-06 02:09:46 -0300

    iniciando configuração do timeout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  // {
+  //   path: '',
+  //   redirectTo: 'home',
+  //   pathMatch: 'full'
+  // },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+    path: 'configuracoes',
+    loadChildren: () => import('./configuracoes/configuracoes.module').then( m => m.ConfiguracoesPageModule)
+  }
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  newMinutes:number = 25;
+  newSeconds:number = 1;
+  minutes:number = this.newMinutes;
+  seconds:number = this.newSeconds;
+  contagem:string;
+  runSecond:number;
+  startClick:number = 0;
+  constructor() {
 
-  constructor() {}
+  }
+
+  validateMaxLenghtHour(hour:number) {
+    if (hour > 60) {
+      this.minutes = 0;
+    }
+  }
+
+  validateMaxLenghtSecond(seconds:number) {
+    if (seconds > 60) {
+      this.seconds = 0;
+    }
+  }
+
+  startCounter () {
+    if (this.startClick == 0) {
+      if (this.seconds > 0) {
+        setTimeout(() => {
+          this.contagem   = this.minutes + ":" + this.seconds;
+          this.seconds    -= 1;
+          this.startClick = 0;
+          this.startCounter()
+
+        }, 1000);
+      } else if (this.minutes > 0) {
+        setTimeout(() => {
+          this.startClick = 0;
+          this.contagem   = this.minutes + ":" + this.seconds;
+          this.minutes    -= 1;
+          // resetando os segundos
+          this.seconds = 59;
+          this.startCounter();
+        }, 1000);
+      }
+      this.startClick = 1;
+    } else {
+      this.contagem = "";
+      this.seconds = 0;
+      this.minutes = 0;
+      this.startClick = 1;
+    }
+  }
 
 }
